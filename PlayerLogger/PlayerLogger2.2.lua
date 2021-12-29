b9ace9b9fd01820522a095bcd0ba626dd08d157f
Davemane42 <davemane43@gmail.com>
2021-12-28 23:59:41 -0500

    screen communication protocol

--- PlayerLogger/PlayerLogger2.2.lua
+++ PlayerLogger/PlayerLogger2.2.lua
@@ -34,11 +34,20 @@ location = "Lobby" -- Name of the Location
 knownUser = {} -- knownUser = {"User1"} -- multiple user knownUser = {"User1","User2","User3"}
 ignoreKnown = true -- Doesn't display known user(s) to prevent screen flooding
 
+version = "2.2"
+
 player = database.getPlayer(unit.getMasterPlayerId())
 latestList = json.decode(dataBank.getStringValue("latest"))
 
 if latestList == nil then latestList={} end
 known = false
+
+comState = nil
+loadIterator = 0
+exit = false
+
+screen.setScriptInput("")
+screen.clearScriptOutput()
 
 function getTime()
     local hoursOffset = 0
@@ -69,34 +78,66 @@ function getTime()
     return (day .. "/" .. month .. " " .. hours .. ":" .. minutes .. ":" .. seconds)
 end
 
-function redraw()
-    local latestHTML = ""
-    local count = 0
-    if latestList ~= {} then
-        for k, v in ipairs(latestList) do
-            local textX = (count>=24 and "rx/2+" or "").."5"
-            local textY = 20+((count%24)*24)
-            count = count+1
-            latestHTML = latestHTML..""..(v[4] and 'setNextFillColor(layer, 0, 1, 0, 1)\n' or "")..'addText(layer, font, "'..string.format('%s %s %s", %s, %s)\n', v[3], v[1], v[2], textX, textY)
-        end
-    end
+local screenVer = dataBank.getStringValue("screenVer")
+if screenVer ~= version then
+    dataBank.setStringValue("screenVer", version)
+    screen.setRenderScript([[local json = require("dkjson")
 
-    screen.setRenderScript([[
 local rx, ry = getResolution()
 local layer = createLayer()
+local front = createLayer()
 local font = loadFont('FiraMono', 20)
+local fontAH, fontDH = getFontMetrics(font)
+local fontSmall = loadFont('FiraMono', 12)
 setDefaultFillColor(layer, 6, 1, 0, 0, 1)
 
-]]..latestHTML..[[
+if jsonData == nil then jsonData = {} end
+if buffer == nil then buffer = "" setOutput("Start") end
+if comState == nil then comState = "" end
 
-local layer = createLayer()
-local font2 = loadFont('RobotoMono-Bold', 12)
-setDefaultFillColor(layer, 6, 1, 1, 1, 1)
-setNextFillColor(layer, 0, 0, 0, 1)
-addBox (layer, rx-155, ry-40, 145, 35)
-addText(layer, font2, 'Location: "]]..location..[["', rx-150, ry-24)
-addText(layer, font2, "Player Logger v2.1", rx-150, ry-12)
-]])
+local input = getInput()
+if input ~= "" then
+    --addText(front, font, getInput(), 5, ry-20)
+    if input == "Start" then
+        comState = "Loading"
+        setOutput("Loading")
+        buffer = ""
+    elseif input == "End" then
+        comState = "End"
+        local tempData = json.decode(buffer)
+        if tempData ~= nil then
+            setOutput("End")
+            jsonData = tempData
+        else
+            setOutput("Error")
+        end
+        buffer = ""
+    elseif comState == "Loading" then
+        buffer = buffer..input
+        setOutput("Loading")
+    end
+end
+
+for k, v in ipairs(jsonData) do
+    local fit = math.floor(ry/fontAH)
+    if k > fit*2 then return end
+    local text = string.format('%s %s %s', v[3], v[1], v[2])
+    local textX = 5+math.floor((k-1)/fit)*rx/2
+    local textY = fontAH*(((k-1)%fit)+1)
+    if v[4] then setNextFillColor(layer, 0, 1, 0, 1) end
+    addText(layer, font, text, textX, textY)
+end
+
+setDefaultFillColor(front, 6, 1, 1, 1, 1)
+setNextFillColor(front, 0, 0, 0, 1)
+addBox (front, rx-155, ry-40, 145, 35)
+addText(front, fontSmall, 'Location: "]]..location..[["', rx-150, ry-24)
+addText(front, fontSmall, "Player Logger v]]..version..[[", rx-150, ry-12)]])
+end
+
+function updateScreenData()
+    screen.setScriptInput("Start")
+    comState = "Start"
 end
 
 for i, v in pairs(knownUser) do
@@ -106,9 +147,10 @@ for i, v in pairs(knownUser) do
 end
 
 if known then
-    if vec3.new(unit.getMasterPlayerRelativePosition()):len() <= 3 then
+    system.print(vec3.new(unit.getMasterPlayerPosition()):len())
+    if vec3.new(unit.getMasterPlayerPosition ()):len() <= 3 then
         system.print("")
-        system.print("Player Logger V2.1 by Davemane42")
+        system.print("Player Logger v"..version.." by Davemane42")
         system.print("Debug enabled, type 'help' to get a list of commands")
         return
     else
@@ -129,9 +171,44 @@ if (known and ignoreKnown) == false then
     dataBank.setStringValue("latest", json.encode(latestList))
 end
 
-redraw()
+updateScreenData()
 
-unit.exit()
+exit = true
+
+--------------------------------------------
+
+------------- system.update() --------------
+
+local output = screen.getScriptOutput()
+if output ~= "" then
+    screen.setScriptInput("")
+    screen.clearScriptOutput()
+
+    if output == "Start" then
+        updateScreenData()
+    end
+
+    if output == "Loading" then
+        local str = json.encode(latestList)
+        local max = math.floor(str:len()/1000)
+
+        if loadIterator > max then
+            loadIterator = 0
+            screen.setScriptInput("End")
+            comState = "End"
+        else
+            local data = str:sub(1+1000*loadIterator, 1000*loadIterator+1000)
+            screen.setScriptInput(data)
+            loadIterator = loadIterator+1
+        end
+    end
+
+    if output == "End" then
+        comState = nil
+    end
+end
+
+if exit and comState == nil then unit.exit() end
 
 --------------------------------------------
 
@@ -147,11 +224,12 @@ end
 if arguments[1] == "clear" then
     dataBank.clear()
     system.print("DataBank Cleared")
-    latestList, unknownList = {}, {}
-    redraw()
-    unit.exit()
+    latestList = {}
+    screen.clearScriptOutput()
+    updateScreenData()
+    exit=true
 elseif arguments[1] == "dump" then
-    screen.setHTML(json.encode(latestList))
+    screen.setRenderScript(json.encode(latestList))
     system.print("")
     system.print("dumped tables to the screen HTML")
     system.print("Right click (on the screen) -> Advanced -> Edit HTML content")
@@ -162,18 +240,18 @@ elseif arguments[1] == "remove" then
         system.print("removed #"..i.."")
         table.remove(latestList, i)
         dataBank.setStringValue("latest", json.encode(latestList))
-        redraw()
+        updateScreenData()
     end
 elseif arguments[1] == "exit" then
-    redraw()
-    unit.exit()
+    updateScreenData()
+    exit=true
 elseif arguments[1] == "help" then
     local help = {
         "'clear' [clear the databank]",
         "'dump' [dump the table as JSON in the HTML so you can copy it]",
+        "'exit' [exit debug mode]",
         "'import' [WIP]",
-        "'remove (indice)' [remove the entry from the list",
-        "'exit' [exit debug mode]"
+        "'remove (indice)' [remove the entry from the list]",
     }
     system.print("")
     for k, v in pairs(help) do
